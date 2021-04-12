13c8e10d6c0e48eb44bab6e7ff63a5933e99a1c2
RVeronica <akinrv16@gmail.com>
2021-04-13 00:26:55 +0300

    refactor:implement function in task 09

--- src/09-sum-digits.js
+++ src/09-sum-digits.js
@@ -10,8 +10,15 @@
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new Error('Not implemented');
+function getSumOfDigits(n) {
+  let sum = 0;
+  const nToString = String(n).split('');
+  for (let i = 0; i < nToString.length; i++) {
+    sum += (+nToString[i]);
+  }
+  if (sum < 10) return sum;
+  const newN = sum;
+  return getSumOfDigits(newN);
 }
 
 module.exports = getSumOfDigits;
