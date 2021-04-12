796360918d2afa4842b804dd9d3992fe4927af00
RVeronica <akinrv16@gmail.com>
2021-04-12 22:45:15 +0300

    refactor:implemet functions in tasks 02 and 03

--- src/03-sort-by-height.js
+++ src/03-sort-by-height.js
@@ -1,5 +1,6 @@
 /**
- * Given an array with heights, sort them except if the value is -1.
+ *
+ *
  *
  * @param {Array} arr
  * @return {Array}
@@ -9,8 +10,47 @@
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new Error('Not implemented');
+function sortByHeight(arr) {
+  const copiedArr = arr;
+  const arrOfMinusOnes = [];
+  let blankPlace;
+  let counter = 0;
+  for (let i = 0; i < copiedArr.length; i++) {
+    if (copiedArr[i] === -1) {
+      counter++;
+    }
+  }
+  if (counter === copiedArr.length) {
+    return copiedArr;
+  }
+  for (let i = 0; i < copiedArr.length; i++) {
+    if (copiedArr[i] === -1) {
+      arrOfMinusOnes.push(i);
+    }
+  }
+  for (let i = 0; i < copiedArr.length; i++) {
+    if (copiedArr[i] === -1) {
+      copiedArr.splice(i, 1);
+      i--;
+    }
+  }
+  for (let i = 0; i < copiedArr.length; i++) {
+    for (let j = 0; j < copiedArr.length; j++) {
+      if (copiedArr[i] < copiedArr[j]) {
+        blankPlace = copiedArr[i];
+        copiedArr[i] = copiedArr[j];
+        copiedArr[j] = blankPlace;
+      }
+    }
+  }
+  for (let i = 0; i < arrOfMinusOnes.length; i++) {
+    for (let j = 0; j < copiedArr.length; j++) {
+      if (j === arrOfMinusOnes[i]) {
+        copiedArr.splice(j, 0, -1);
+      }
+    }
+  }
+  return copiedArr;
 }
 
 module.exports = sortByHeight;
